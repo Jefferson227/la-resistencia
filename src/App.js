c7799ee1efb300e2557143385f16e51d68c52543
Jefferson Matheus <jefferson227@gmail.com>
2020-05-28 13:29:05 -0300

    Add config for eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import firebase from "firebase";
+import React from 'react';
+import firebase from 'firebase';
+import logo from './logo.svg';
+import './App.css';
 
 // Set the configuration for your app
 // TODO: Replace with your project's config object
-var config = {
+const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 firebase.initializeApp(config);
 
-// Get a reference to the database service
-var database = firebase.database();
-console.log(database);
-
 function getPlayers() {
-  let element = <ul></ul>;
-  var player = firebase.database().ref("matches/-M8JR_cNCE_t5ntrDc10/players");
-  player.on("value", function (snapshot) {
-    let players = snapshot.val();
+  let element = <ul />;
+  const player = firebase.database().ref('matches/-M8JR_cNCE_t5ntrDc10/players');
+
+  player.on('value', (snapshot) => {
+    const players = snapshot.val();
 
     element = (
       <ul>
-        {Object.keys(players).map((id, index) => (
-          <li key={index}>players[id]</li>
+        {Object.keys(players).map((id) => (
+          <li key={id}>players[id]</li>
         ))}
       </ul>
     );
@@ -37,34 +33,31 @@ function getPlayers() {
 }
 
 function addNewMatch() {
-  const matchId = firebase.database().ref("matches").push().key;
+  const matchId = firebase.database().ref('matches').push().key;
   const match = {
     totalPlayers: 0,
     currentMission: 1,
     playersInMission: 2,
   };
 
-  let updates = {};
+  const updates = {};
   updates[`/matches/${matchId}`] = match;
 
   firebase.database().ref().update(updates);
-  console.log("match created successfully");
+  console.log('match created successfully');
 }
 
 function addPlayerToMatch(name) {
-  const playerId = firebase
-    .database()
-    .ref("matches/-M8JR_cNCE_t5ntrDc10/players")
-    .push().key;
+  const playerId = firebase.database().ref('matches/-M8JR_cNCE_t5ntrDc10/players').push().key;
   const player = {
     name,
   };
 
-  let updates = {};
+  const updates = {};
   updates[`/matches/-M8JR_cNCE_t5ntrDc10/players/${playerId}`] = player;
 
   firebase.database().ref().update(updates);
-  console.log("player added successfully");
+  console.log('player added successfully');
 }
 
 function App() {
@@ -73,22 +66,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <button className="new-match" onClick={() => addNewMatch()}>
+          <button type="button" className="new-match" onClick={() => addNewMatch()}>
             Create a new match
           </button>
         </p>
         <p>
-          <button
-            className="new-match"
-            onClick={() => addPlayerToMatch("megaman x")}
-          >
+          <button type="button" className="new-match" onClick={() => addPlayerToMatch('megaman x')}>
             Add player to match
           </button>
         </p>
-        <div>Players: {getPlayers()}</div>
+        <div>
+          Players:
+          {getPlayers()}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
