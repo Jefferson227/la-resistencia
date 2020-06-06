29d26a3522fce7a54a226ccf4d91e5542cd2f27a
Jefferson Matheus <jefferson227@gmail.com>
2020-06-06 16:05:30 -0300

    Simplify the test for firebase database

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import firebaseDatabase from './utils/firebaseUtils';
 import './App.css';
 
 class App extends React.Component {
+  static addNewPlayer() {
+    const name = document.getElementById('player-name').value;
+    const playerId = firebaseDatabase.ref('players').push().key;
+    const player = {
+      name,
+    };
+
+    const updates = {};
+    updates[`/players/${playerId}`] = player;
+
+    firebaseDatabase.ref().update(updates);
+  }
+
   constructor(props) {
     super(props);
 
@@ -16,47 +29,9 @@ class App extends React.Component {
     this.getPlayers();
   }
 
-  addNewMatch() {
-    const matchId = firebaseDatabase.ref('matches').push().key;
-    const match = {
-      totalPlayers: 0,
-      currentMission: 1,
-      playersInMission: 2,
-    };
-
-    const updates = {};
-    updates[`/matches/${matchId}`] = match;
-
-    firebaseDatabase.ref().update(updates);
-    console.log('match created successfully');
-  }
-
-  addPlayerToMatch() {
-    const name = document.getElementById('player-name').value;
-    const playerId = firebaseDatabase.ref('matches/-M8JR_cNCE_t5ntrDc10/players').push().key;
-    const player = {
-      name,
-    };
-
-    const updates = {};
-    updates[`/matches/-M8JR_cNCE_t5ntrDc10/players/${playerId}`] = player;
-
-    firebaseDatabase.ref().update(updates);
-    console.log('player added successfully');
-  }
-
   getPlayers() {
-    let players = [];
-
-    FirebaseService.getDataList('matches/-M8JR_cNCE_t5ntrDc10/players', (items) => {
-      document.getElementById('player-list').innerHTML = '';
-      this.setState({ players: [] });
-
-      items.forEach((item) => {
-        players.push(item);
-      });
-
-      this.setState({ players: players });
+    FirebaseService.getDataList('players', (players) => {
+      this.setState({ players });
     });
   }
 
@@ -64,32 +39,21 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <p>
-            <button type="button" className="new-match" onClick={() => this.addNewMatch()}>
-              Create a new match
-            </button>
-          </p> */}
           <div>
-            <input type="text" id="player-name" placeholder="New player name"></input>
-            <button type="button" onClick={() => this.addPlayerToMatch()}>
+            <input type="text" id="player-name" placeholder="New player name" />
+            <button type="button" onClick={() => App.addNewPlayer()}>
               Add new player
             </button>
           </div>
-          <p>
-            <button
-              type="button"
-              className="new-match"
-              onClick={() => this.addPlayerToMatch('megaman x')}
-            >
-              Add player to match
-            </button>
-          </p>
           <div>
             Players:
             <ul id="player-list">
-              {this.state.players.map((player) => {
-                return <li key={player.key}>{player.name}</li>;
-              })}
+              {
+                // eslint-disable-next-line react/destructuring-assignment
+                this.state.players.map(({ key, name }) => {
+                  return <li key={key}>{name}</li>;
+                })
+              }
             </ul>
           </div>
         </header>
