84f82373dade45d212970a4395e86aaa28100d57
Jefferson Matheus <jefferson227@gmail.com>
2020-06-21 18:19:13 -0300

    Install react-router
    Also it was installed the Match component to test the routing.

---
+++ src/components/match/Match.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Match.css';
+
+const Match = () => {
+  return <div>olar</div>;
+};
+
+export default Match;
--- src/components/matches/Matches.js
+++ src/components/matches/Matches.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import FirebaseService from '../../services/firebaseServices';
 // import firebaseDatabase from '../../utils/firebaseUtils';
 import './Matches.css';
+import Match from '../match/Match';
 
 class Matches extends React.Component {
   constructor(props) {
@@ -32,9 +34,23 @@ class Matches extends React.Component {
         <div className="Matches-list">
           {this.state.matches.map(({ key }) => {
             return (
-              <Button variant="contained" key={key}>
-                {key}
-              </Button>
+              <div>
+                <Router>
+                  <Link to="/match" key={key}>
+                    {key}
+                  </Link>
+
+                  <Switch>
+                    <Route path="/match">
+                      <Match />
+                    </Route>
+                  </Switch>
+                </Router>
+
+                {/* <Button variant="contained" key={key}>
+                  {key}
+                </Button> */}
+              </div>
             );
           })}
         </div>
